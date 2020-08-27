1e82f9bd9341dc04fbaddad56916e0902449ac19
Luis Guilherme Bezerra Bispo <luguilherm@engdb.infra>
2020-08-27 15:27:44 -0300

    Criação do ReservaController/Criação de Rotas

---
+++ src/controller/ReservaController.js
@@ -0,0 +1,66 @@
+const connection = require('../database/connection');
+
+
+module.exports = {
+
+    async index(request, response) {
+        const reservas = await connection('tbReserva').select('*')
+        return response.status(200).json(reservas);
+    },
+
+    async create(request, response) {
+        const {
+            dataEntrada,
+            dataSaida
+        } = request.body;
+        try {
+            await connection('tbReserva').insert({
+                dataEntrada,
+                dataSaida
+            })
+            return response.status(200).send({
+                message: 'Salvo com sucesso!'
+            })
+        } catch (error) {
+            console.log(error);
+            return response.status(500).send({
+                message: 'ERROR: ' + error
+            })
+        }
+
+    },
+
+    async delete(request, response) {
+        const id = request.body.codReserva;
+
+        try {
+            await connection('tbReserva').where('codReserva', id).delete()
+            response.status(200).send({
+                message: 'Excluido com sucesso.'
+            })
+        } catch (error) {
+            console.log(error);
+            return response.status(500).send({
+                message: 'ERROR: ' + error
+            })
+        }
+    },
+
+    async update(request, response) {
+        const {
+            codReserva,
+            dataEntrada,
+            dataSaida
+        } = request.body;
+
+        try {
+            const reserva = await connection('tbReserva').where('codReserva', codReserva).update(dataEntrada, dataSaida)
+            response.status(200).json(reserva)
+        } catch (error) {
+            console.log(error);
+            return response.status(500).send({
+                message: 'ERROR: ' + error
+            })
+        }
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,21 @@ const routes = express.Router();
 
 const HomeController = require('./controller/HomeController')
 const QuartoController = require('./controller/QuartoController')
+const ReservaController = require('./controller/ReservaController')
 
+//GET
 routes.get('/', HomeController.index)
 routes.get('/quartos', QuartoController.index)
+routes.get('/reservas', ReservaController.index)
+//DELETE
 routes.delete('/delete/quarto', QuartoController.delete)
+routes.delete('/delete/reserva', ReservaController.delete)
+//POST
 routes.post('/quarto', QuartoController.create)
+routes.post('/reserva', ReservaController.create)
+//PUT
 routes.put('/update/quarto', QuartoController.update)
+routes.put('/update/reserva', ReservaController.update)
+
 
 module.exports = routes;
